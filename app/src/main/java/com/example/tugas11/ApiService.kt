121f701ef08423c0813edf3b4a6a40ca21c7539e
Reyhanaku <hardiyusuf110@gmail.com>
2026-07-19 10:13:25 +0700

    feat: implement Moshi data model, Retrofit ApiService, and Network singleton

---
+++ app/src/main/java/com/example/tugas11/ApiService.kt
@@ -0,0 +1,15 @@
+﻿package com.example.tugas11
+
+import retrofit2.http.GET
+
+/**
+ * Interface untuk mendefinisikan endpoint RESTful Web Service menggunakan Retrofit.
+ */
+interface ApiService {
+    /**
+     * Mengambil daftar foto dari server.
+     * Fungsi bersifat 'suspend' agar dapat dipanggil di dalam coroutine tanpa memblokir thread UI.
+     */
+    @GET("photos")
+    suspend fun getPhotos(): List<MarsPhoto>
+}
---
+++ app/src/main/java/com/example/tugas11/MarsPhoto.kt
@@ -0,0 +1,16 @@
+﻿package com.example.tugas11
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+/**
+ * Data class yang mewakili respons JSON dari web service.
+ * Anotasi @JsonClass menginstruksikan Moshi untuk membuat adapter otomatis saat kompilasi.
+ */
+@JsonClass(generateAdapter = true)
+data class MarsPhoto(
+    val id: String,
+    
+    // Menggunakan @Json untuk memetakan nama key di JSON ("img_src") ke variabel Kotlin ("imgSrcUrl")
+    @Json(name = "img_src") val imgSrcUrl: String
+)
